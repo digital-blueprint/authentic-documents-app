ae55c1917f802cbef59a8a3cd8d7c1f55d912f1c
Christina Tögl <toegl@tugraz.at>
2020-09-15 17:08:05 +0200

    Add content, buttons, click events and CSS

--- src/dbp-authentic-image-request.js
+++ src/dbp-authentic-image-request.js
@@ -5,6 +5,7 @@ import * as commonUtils from 'dbp-common/utils';
 import {Button, Icon, MiniSpinner} from 'dbp-common';
 import * as commonStyles from 'dbp-common/styles';
 import {TextSwitch} from './textswitch.js';
+import { send } from 'dbp-common/notification';
 
 const i18n = createI18nInstance();
 
@@ -17,8 +18,10 @@ class AuthenticImageRequest extends ScopedElementsMixin(LitElement) {
         this.given_name = '';
         this.family_name = '';
         this.fullResponse = '';
-        this.entryPointUrl = commonUtils.getAPiUrl();
         this.responseFromServer = '';
+        this.items = [];
+        this.disableBtns = false;
+        this.gridContainer = "display:none";
     }
 
     static get scopedElements() {
@@ -38,7 +41,10 @@ class AuthenticImageRequest extends ScopedElementsMixin(LitElement) {
             given_name: { type: String, attribute: false },
             family_name: { type: String, attribute: false },
             fullResponse: { type: String, attribute: false },
-            responseFromServer: { type: String, attribute: false }
+            responseFromServer: { type: String, attribute: false },
+            items: { type: Array, attribute: false },
+            disableBtns: { type: Boolean, attribute: false },
+            gridContainer: { type: String, attribute: false },
         };
     }
 
@@ -59,6 +65,21 @@ class AuthenticImageRequest extends ScopedElementsMixin(LitElement) {
 
     }
 
+    parseAvailableDocumentTypes() {
+        let numTypes = parseInt(this.responseFromServer['hydra:totalItems']);
+        if (isNaN(numTypes)) {
+            numTypes = 0;
+        }
+
+        for (let i = 0; i < numTypes; i++ ) {
+            let entry = this.responseFromServer['hydra:member'][i];
+
+            if (entry['availabilityStatus'] !== 'not_available') {
+                this.items[i] = entry['urlSafeAttribute'];
+            }
+        }
+    }
+
     async httpGetAsync(url, options)
     {
         let response = await fetch(url, options).then(result => {
@@ -68,11 +89,10 @@ class AuthenticImageRequest extends ScopedElementsMixin(LitElement) {
                 console.log("fetch error:", error);
             });
 
-        return await response;
-
+        return response;
     }
 
-    parseJwt (token) {
+    parseJwt(token) {
         var base64Url = token.split('.')[1];
         var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
         var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
@@ -84,6 +104,8 @@ class AuthenticImageRequest extends ScopedElementsMixin(LitElement) {
 
     async retrieveToken() {
         let response;
+
+        //Retrieve token from KC
         const options_get_access_token = {
             headers: {
                 Authorization: "Bearer " + window.DBPAuthToken
@@ -93,29 +115,57 @@ class AuthenticImageRequest extends ScopedElementsMixin(LitElement) {
 
         if (response && response.access_token) {
             this.access_token = response.access_token;
-            this.fullRespons = this.parseJwt(response.id_token);
-            this.family_name = this.fullRespons.family_name;
-            this.given_name = this.fullRespons.given_name;
+            this.fullResponse = this.parseJwt(response.id_token);
+            this.family_name = this.fullResponse.family_name;
+            this.given_name = this.fullResponse.given_name;
+            this.realFullResponse = response;
         }
 
         console.log(response);
 
-        const options_send_api_request = {
-            method: 'POST',
+        //Retrieve a list of available document types
+        const options_send_api_request_doc_types = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/ld+json',
+                'Authorization': 'Bearer ' + window.DBPAuthToken
+            }};
+
+        const url = this.entryPointUrl + '/authentic_document_types?page=1&token=' + 'photo-jpeg-available-token'; //TODO: replace token
+
+        if (this.access_token !== '') {
+            this.responseFromServer = await this.httpGetAsync(url, options_send_api_request_doc_types);
+        }
+
+        this.parseAvailableDocumentTypes();
+        this.gridContainer = "display:flex";
+
+        /*
+        //Retrieve document
+        const options_send_api_request_for_type = {
+            method: 'GET',
             headers: {
                 'Content-Type': 'application/ld+json',
                 'Authorization': 'Bearer ' + window.DBPAuthToken,
-            },
-            body: JSON.stringify({
-                'token': this.access_token,
-                'type': 'image',
-            })};
+            }
+        };
 
-        const url = this.entryPointUrl + '/authentic_document_requests';
+        const url_document = this.entryPointUrl + '/authentic_documents/' + 'cGhvdG8tanBlZy1hdmFpbGFibGU%253D' + '?token=' + 'photo-jpeg-available-token'; //TODO: replace id; token
 
         if (this.access_token !== '') {
-            this.responseFromServer = await this.httpGetAsync(url, options_send_api_request);
+            this.responseFromServer2 = await this.httpGetAsync(url_document, options_send_api_request_for_type);
         }
+        */
+    }
+
+    buttonPressSuccessMessage() {
+        this.disableBtns = true;
+        send({
+            "summary": i18n.t('authentic-image-request.request-success-title'),
+            "body": i18n.t('authentic-image-request.request-success-body', { name: this.name }),
+            "type": "success",
+            "timeout": 10,
+        });
     }
 
     static get styles() {
@@ -125,6 +175,33 @@ class AuthenticImageRequest extends ScopedElementsMixin(LitElement) {
             ${commonStyles.getGeneralCSS(false)}
             ${commonStyles.getButtonCSS()}
             ${commonStyles.getNotificationCSS()}
+            
+            #docs {
+                display: block;
+                margin-block-start: 1em;
+                margin-block-end: 1em;
+                margin-inline-start: 0px;
+                margin-inline-end: 0px;
+            }
+            
+            .border {
+                border-top: 1px solid black;
+            }
+            
+            #grid-container {
+                margin-top: 2rem;
+                padding-top: 2rem;
+                display: flex;
+                flex-flow: column;
+            }
+            
+            h2:first-child {
+                margin-top: 0px;
+            }
+
+            h2 {
+                margin-bottom: 10px;
+            }
         `;
     }
 
@@ -132,12 +209,14 @@ class AuthenticImageRequest extends ScopedElementsMixin(LitElement) {
         return html`
            <h2>${i18n.t('authentic-image-request.title')}</h2>
            <p>${i18n.t('authentic-image-request.description')}</p>
-           <button class="button is-primary" @click="${this.retrieveToken}" title="${i18n.t('authentic-image-request.request-button')}">retrieve token</button>
+           <button class="button is-primary" @click="${this.retrieveToken}" title="${i18n.t('authentic-image-request.request-button')}">${i18n.t('authentic-image-request.retrieve-token')}</button>
            
-           <h2>Hallo ${this.given_name} ${this.family_name}! </h2>
-           <pre><code> ${JSON.stringify(this.responseFromServer, undefined, 4)} </code></pre>
-           <p> ${this.access_token} </p><br>
-           <pre><code> ${JSON.stringify(this.fullRespons, undefined, 4)} </code></pre>
+           <div id="grid-container" class="border" style="${this.gridContainer}">
+                <h2 id="doc-headline" >${i18n.t('authentic-image-request.available-documents')} ${this.given_name} ${this.family_name}:</h2>
+                <div id="docs">
+                    ${this.items.map(i => html`<p>${i}</p><button class="button is-primary" @click="${this.buttonPressSuccessMessage}" title="${i}" ?disabled="${this.disableBtns}" >${i18n.t('authentic-image-request.request-document')}</button>`)}
+                </div>
+           </div>
         `;
     }
 }
