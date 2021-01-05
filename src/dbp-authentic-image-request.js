822a962f76fc5502922b44ea1abc905157d20a15
Christoph Reiter <reiter.christoph@gmail.com>
2021-01-05 15:56:23 +0100

    Inject the demo token so we get some demo responses
    Only used on the server for now, not locally.

--- src/dbp-authentic-image-request.js
+++ src/dbp-authentic-image-request.js
@@ -6,6 +6,7 @@ import {Button, Icon, MiniSpinner} from '@dbp-toolkit/common';
 import * as commonStyles from '@dbp-toolkit/common/styles';
 import {TextSwitch} from './textswitch.js';
 import { send } from '@dbp-toolkit/common/notification';
+import environment from 'consts:environment';
 
 const i18n = createI18nInstance();
 
@@ -122,6 +123,10 @@ class AuthenticImageRequest extends ScopedElementsMixin(LitElement) {
         response = await this.httpGetAsync('https://auth-dev.tugraz.at/auth/realms/tugraz/broker/eid-oidc/token', options_get_access_token);
 
         if (response && response.access_token) {
+            // XXX: Removes this, just used for demo purposes
+            if (environment !== 'local')
+                response.access_token = 'photo-jpeg-available-token';
+
             this.access_token = response.access_token;
             this.fullResponse = this.parseJwt(response.id_token);
             this.family_name = this.fullResponse.family_name;
